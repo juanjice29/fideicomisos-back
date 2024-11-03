a3b8339c0a7b5e4f523986c7b72c2b5f93f5d501
juanjice29 <jm.jimenezc12@uniandes.edu.co>
2024-11-03 17:13:22 -0500

    testing sonarqube

--- sgc_backend/public/views.py
+++ sgc_backend/public/views.py
@@ -20,7 +20,7 @@ class IndexView(APIView):
     permission_classes = [AllowAny]
     def get(self, request, format=None):
         content = {
-            'wmsg':"Welcome to SGC Api Fiduciaria"
+            'wmsg':"Welcome to SGC Api Fiduciaria, prueba de sonar qube"
         }
         return Response(content)
 
